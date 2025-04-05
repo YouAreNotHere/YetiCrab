9031819b6e3ec85ac849e86b1a007ece452f4541
YouAreNotHere <malferety@gmail.com>
2025-04-05 23:03:22 +0500

    После сдачи: внедрил валидацию

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,3 +1,49 @@
+// import { Module } from '@nestjs/common';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { AttractionsModule } from './attractions/attractions.module';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
+// import { Response } from 'express'; // Импортируем тип Response
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//     }),
+//     ServeStaticModule.forRoot({
+//       rootPath: join(__dirname, '..', 'uploads'),
+//       serveRoot: '/uploads',
+//       serveStaticOptions: {
+//         setHeaders: (res: Response, path: string) => {
+//           if (path.endsWith('.jpg') || path.endsWith('.jpeg')) {
+//             res.setHeader('Content-Type', 'image/jpeg');
+//           } else if (path.endsWith('.png')) {
+//             res.setHeader('Content-Type', 'image/png');
+//           }
+//         },
+//       },
+//     }),
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       useFactory: (configService: ConfigService) => ({
+//         type: 'mysql',
+//         host: configService.get<string>('DB_HOST'),
+//         port: configService.get<number>('DB_PORT'),
+//         username: configService.get<string>('DB_USERNAME'),
+//         password: configService.get<string>('DB_PASSWORD'),
+//         database: configService.get<string>('DB_NAME'),
+//         entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//         synchronize: true,
+//         logging: true,
+//       }),
+//       inject: [ConfigService],
+//     }),
+//     AttractionsModule,
+//   ],
+// })
+// export class AppModule {}
+
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -28,11 +74,11 @@ import { Response } from 'express'; // Импортируем тип Response
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
+        host: 'localhost',
         port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
+        username: 'YouAreNotHere',
+        password: '123456',
+        database: 'world',
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
         logging: true,
