5a9f7fc3481622a34252403c7e878b7eb6c959a6
YouAreNotHere <malferety@gmail.com>
2025-04-06 18:44:09 +0500

    После сдачи: minor

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+// import { seedAttractions} from './attractions/attractions.seed'
 import morgan from 'morgan';
 
 async function bootstrap() {
@@ -29,5 +30,6 @@ async function bootstrap() {
   await app.listen(8081, () => {
     console.log('Server is running on http://localhost:8081');
   });
+  // await seedAttractions();
 }
 bootstrap();
